db76cad1967cfd238f1f3cffbfd80dfd79987b2b
MvITA5 <vujinovicmihajlo05@gmail.com>
2026-05-25 21:49:24 +0200

    modified main.py

--- main.py
+++ main.py
@@ -6,13 +6,9 @@ from product_manager import ProductManager
 def main():
   pm = ProductManager()
 
-  pm.add_product(Product("Laptop", 1200.00, 5))
+  pm.add_product(Product("Desktop PC", 1200.00, 5))
   pm.add_product(Product("Mouse", 25.50, 10))
   pm.add_product(Product("Keyboard", 45.99, 7))
-
-  print("Available products:")
-  pm.display_products()
-  print(f"Total inventory value: ${pm.total_inventory_value():.2f}\n")
 
 if __name__ == "__main__":
   main()
