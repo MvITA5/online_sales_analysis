e7fd22606ae2e9f59c837bc4f4fc5948eeaba2a0
MvITA5 <vujinovicmihajlo05@gmail.com>
2026-05-25 21:10:58 +0200

    Added new function in product_manager.py that removes product by name

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ from product import Product
 from product_manager import ProductManager
 
 def main():
-pm = ProductManager()
+  pm = ProductManager()
 
-pm.add_product(Product("Laptop", 1200.00, 5))
-pm.add_product(Product("Mouse", 25.50, 10))
-pm.add_product(Product("Keyboard", 45.99, 7))
+  pm.add_product(Product("Laptop", 1200.00, 5))
+  pm.add_product(Product("Mouse", 25.50, 10))
+  pm.add_product(Product("Keyboard", 45.99, 7))
 
-print("Available products:")
-pm.display_products()
-print(f"Total inventory value: ${pm.total_inventory_value():.2f}\n")
+  print("Available products:")
+  pm.display_products()
+  print(f"Total inventory value: ${pm.total_inventory_value():.2f}\n")
 
 if __name__ == "__main__":
-main()
+  main()
--- product_manager.py
+++ product_manager.py
@@ -3,18 +3,26 @@
 from product import Product
 
 class ProductManager:
-def __init__(self):
-self.products = []
+  def __init__(self):
+    self.products = []
 
-def add_product(self, product):
-self.products.append(product)
+  def add_product(self, product):
+    self.products.append(product)
 
-def display_products(self):
-if not self.products:
-print("No products available.")
-for product in self.products:
-product.display_info()
+  def display_products(self):
+     if not self.products:
+       print("No products available.")
+     for product in self.products:
+        product.display_info()
 
-def total_inventory_value(self):
-total = sum(product.price * product.quantity for product in self.products)
-return total
+  def total_inventory_value(self):
+    total = sum(product.price * product.quantity for product in self.products)
+    return total
+
+  def remove_product_by_name(self, name):
+     original_len = len(self.products)
+     self.products = [product for product in self.products if product.name != name]
+     if len(self.products) < original_len:
+        print(f"Product '{name}' removed.")
+     else:
+        print(f"Product '{name}' not found.")
