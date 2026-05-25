411b9902fd695bb6764906c9653efa9a4287fbb4
MvITA5 <vujinovicmihajlo05@gmail.com>
2026-05-25 21:35:04 +0200

    Created a Cart class and modified main.py.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from product import Product
 from product_manager import ProductManager
+from cart import Cart
 
 def main():
   pm = ProductManager()
@@ -13,6 +14,14 @@ def main():
   print("Available products:")
   pm.display_products()
   print(f"Total inventory value: ${pm.total_inventory_value():.2f}\n")
+
+  cart = Cart()
+  for i in range(min(3, len(pm.products))):
+    cart.add_product(pm.products[i])
+
+  print("Shopping Cart:")
+  cart.display_cart()
+  print(f"Total payment due: ${cart.total_payment():.2f}")
 
 if __name__ == "__main__":
   main()
